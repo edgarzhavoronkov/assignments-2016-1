f1f9487e4c483bc6d23268df1b305bbdf5f0dd5d
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-03-07 11:36:07 +0300

    fixes

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,144 +1,131 @@
 package ru.spbau.mit;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Created by edgar
  * on 16.02.16.
  */
 public class StringSetImpl implements StringSet {
-    private static class StringSetNode {
-        char symbol;
-        String currentString;
-        StringSetNode parent;
-        Map<Character, StringSetNode> children;
-        int prefixCounter;
-        boolean isFinal;
-
-        public StringSetNode(
-                char symbol,
-                String currentString,
-                StringSetNode parent,
-                Map<Character, StringSetNode> children,
-                boolean isFinal) {
-            this.symbol = symbol;
-            this.currentString = currentString;
-            this.parent = parent;
-            this.children = children;
-            this.isFinal = isFinal;
-        }
-    }
-
     private StringSetNode root;
-    private int size;
 
     public StringSetImpl() {
-        size = 0;
-        this.root = new StringSetNode(
-                ' ',
-                "",
-                null,
-                new HashMap<Character, StringSetNode>(),
-                false);
-        root.prefixCounter = 0;
+        root = new StringSetNode();
     }
 
     @Override
     public boolean add(String element) {
         if (this.contains(element)) {
             return false;
-        } else {
-            StringSetNode currentNode = root;
-            int i = 0;
-            int len = element.length();
-            while (i < len) {
-                char currentSymbol = element.charAt(i);
-                StringSetNode node = currentNode.children.get(currentSymbol);
-                if (node != null) {
-                    currentNode = node;
-                } else {
-                    StringSetNode child = new StringSetNode(
-                            currentSymbol,
-                            element.substring(0, i + 1),
-                            currentNode,
-                            new HashMap<Character, StringSetNode>(),
-                            false);
-                    currentNode.children.put(currentSymbol, child);
-                    currentNode = child;
-                }
-                i++;
-            }
-            currentNode.isFinal = true;
-            size++;
-            while (currentNode.parent != null) {
-                currentNode.prefixCounter += 1;
-                currentNode = currentNode.parent;
-            }
-            currentNode.prefixCounter += 1;
-            return true;
         }
-
+        StringSetNode currentNode = getLastNode(element, Action.ADD);
+        currentNode.setSize(currentNode.getSize() + 1);
+        currentNode.setFinal(true);
+        return true;
     }
 
     @Override
     public boolean contains(String element) {
-        StringSetNode currentNode = root;
-        int i = 0;
-        int len = element.length();
-        while (i < len) {
-            char currentSymbol = element.charAt(i);
-            StringSetNode node = currentNode.children.get(currentSymbol);
-            if (node != null) {
-                currentNode = node;
-                i++;
-            } else {
-                return false;
-            }
-        }
-        return currentNode.isFinal;
+        StringSetNode currentNode = getLastNode(element, Action.DEFAULT);
+        return currentNode != null && currentNode.isFinal();
     }
 
     @Override
     public boolean remove(String element) {
-        if (this.contains(element)) {
-            StringSetNode currentNode = getLastNode(element);
-            currentNode.isFinal = false;
-            while (currentNode.parent != null) {
-                currentNode.prefixCounter -= 1;
-                currentNode = currentNode.parent;
-            }
-            currentNode.prefixCounter -= 1;
-            size--;
-            return true;
-        } else {
+        if (!this.contains(element)) {
             return false;
         }
+        StringSetNode currentNode = getLastNode(element, Action.REMOVE);
+        currentNode.setFinal(false);
+        currentNode.setSize(currentNode.getSize() - 1);
+        return true;
     }
 
     @Override
     public int size() {
-        return size;
+        return root.getSize();
     }
 
     @Override
     public int howManyStartsWithPrefix(String prefix) {
-        StringSetNode currentNode = getLastNode(prefix);
-        return currentNode.prefixCounter;
+        StringSetNode currentNode = getLastNode(prefix, Action.DEFAULT);
+        return currentNode.size;
     }
 
-    private StringSetNode getLastNode(String element) {
+    private StringSetNode getLastNode(String element, Action action) {
         StringSetNode currentNode = root;
-        int i = 0;
         int len = element.length();
-        while (i < len) {
+        for (int i = 0; i < len && currentNode != null; ++i) {
             char currentSymbol = element.charAt(i);
-            StringSetNode node = currentNode.children.get(currentSymbol);
-            if (node != null) {
-                currentNode = node;
-                i++;
+            switch (action) {
+                case ADD: {
+                    currentNode.setSize(currentNode.getSize() + 1);
+                    StringSetNode child = currentNode.getChild(currentSymbol);
+                    if (child == null) {
+                        child = new StringSetNode();
+                        currentNode.setChild(currentSymbol, child);
+                    }
+                    currentNode = child;
+                    break;
+                }
+                case REMOVE: {
+                    currentNode.setSize(currentNode.getSize() - 1);
+                    StringSetNode child = currentNode.getChild(currentSymbol);
+                    if (child.getSize() == 0) {
+                        currentNode.setChild(currentSymbol, null);
+                    }
+                    currentNode = child;
+                    break;
+                }
+                case DEFAULT: {
+                    currentNode = currentNode.getChild(currentSymbol);
+                    break;
+                }
             }
         }
         return currentNode;
+    }
+
+    private enum Action {ADD, REMOVE, DEFAULT}
+
+    private static class StringSetNode {
+        private final static int ALPHABET_SIZE = 'z' - 'a' + 1;
+
+        private StringSetNode[] children = new StringSetNode[2 * ALPHABET_SIZE];
+        private boolean isFinal;
+        private int size;
+
+        public StringSetNode() {
+            this.isFinal = false;
+            this.size = 0;
+        }
+
+        public StringSetNode getChild(char c) {
+            if (Character.isLowerCase(c)) {
+                return children[c - 'a'];
+            }
+            return children[ALPHABET_SIZE + Character.toLowerCase(c) - 'a'];
+        }
+
+        public void setChild(char c, StringSetNode val) {
+            if (Character.isLowerCase(c)) {
+                children[c - 'a'] = val;
+            }
+            children[ALPHABET_SIZE + Character.toLowerCase(c) - 'a'] = val;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public boolean isFinal() {
+            return isFinal;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+
+        public void setFinal(boolean aFinal) {
+            isFinal = aFinal;
+        }
     }
 }
