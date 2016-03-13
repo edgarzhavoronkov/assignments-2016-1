c221b13a885fd6871ac0820eb2781135be12419b
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-03-13 15:03:14 +0300

    Second commit WITH serialization

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,5 +1,6 @@
 package ru.spbau.mit;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -57,12 +58,12 @@ public class StringSetImpl implements StringSet, StreamSerializable {
 
     @Override
     public void serialize(OutputStream out) {
-        //TODO
+        root.serialize(out);
     }
 
     @Override
     public void deserialize(InputStream in) {
-        //TODO
+        root.deserialize(in);
     }
 
     private StringSetNode getLastNode(String element, Action action) {
@@ -97,14 +98,15 @@ public class StringSetImpl implements StringSet, StreamSerializable {
 
     private static class StringSetNode implements StreamSerializable {
         private final static int ALPHABET_SIZE = 'z' - 'a' + 1;
+        private final static int BUFFER_SIZE = Integer.BYTES + 1;
 
-        private StringSetNode[] children = new StringSetNode[2 * ALPHABET_SIZE];
-        private boolean isFinal;
         private int size;
+        private boolean isFinal;
+        private StringSetNode[] children = new StringSetNode[2 * ALPHABET_SIZE];
 
         public StringSetNode() {
-            isFinal = false;
             size = 0;
+            isFinal = false;
         }
 
         public StringSetNode getChild(char c) {
@@ -140,12 +142,51 @@ public class StringSetImpl implements StringSet, StreamSerializable {
 
         @Override
         public void serialize(OutputStream out) {
-            //TODO
+            try {
+                byte[] bytesToWrite = new byte[]{
+                        (byte) ((size >> 24) & 0xff),
+                        (byte) ((size >> 16) & 0xff),
+                        (byte) ((size >> 8) & 0xff),
+                        (byte) ((size) & 0xff),
+                        (byte) (isFinal ? 0x01 : 0x00)
+                };
+                out.write(bytesToWrite);
+                for (StringSetNode node : children) {
+                    if (node != null) {
+                        out.write(new byte[]{0x10});
+                        node.serialize(out);
+                    } else {
+                        out.write(new byte[]{0x11});
+                    }
+                }
+            } catch (IOException e) {
+                throw new SerializationException();
+            }
         }
 
         @Override
         public void deserialize(InputStream in) {
-            //TODO
+            try {
+                byte[] buffer1 = new byte[BUFFER_SIZE];
+                //FIXME: use one buffer?
+                byte[] buffer2 = new byte[1];
+                in.read(buffer1);
+                size = (0xff & buffer1[0]) << 24 |
+                        (0xff & buffer1[1]) << 16 |
+                        (0xff & buffer1[2]) << 8 |
+                        (0xff & buffer1[3]);
+                isFinal = buffer1[4] == 0x01;
+
+                for (int i = 0; i < 2 * ALPHABET_SIZE; ++i) {
+                    in.read(buffer2);
+                    if (buffer2[0] == 0x10) {
+                        children[i] = new StringSetNode();
+                        children[i].deserialize(in);
+                    }
+                }
+            } catch (IOException e) {
+                throw new SerializationException();
+            }
         }
     }
 }
