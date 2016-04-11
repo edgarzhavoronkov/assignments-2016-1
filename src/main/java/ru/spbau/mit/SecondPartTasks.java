8ed2f984eb4bd008d7bd2c67a60af647c9772d0b
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-04-11 12:54:34 +0300

    Hw4

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,15 +1,40 @@
 package ru.spbau.mit;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
+
+    private static final long NUMBER_OF_EXPERIMENTS = 10000;
 
     private SecondPartTasks() {}
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths
+                .stream()
+                .flatMap(SecondPartTasks::getLines)
+                .filter(l -> l.contains(sequence))
+                .collect(Collectors.toList());
+    }
+
+    private static Stream<String> getLines(String path) {
+        Stream<String> lines;
+        try {
+            lines = Files.lines(Paths.get(path));
+            return lines;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +42,64 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        Random random = new Random(System.currentTimeMillis());
+        final double centerX = 0.5;
+        final double centerY = 0.5;
+        final double radius = 0.5;
+
+        Supplier<Double> shots = random::nextDouble;
+
+        Stream<Boolean> results = Stream.generate(
+                () -> {
+                    final double x = shots.get();
+                    final double y = shots.get();
+                    return (x - centerX) * (x - centerX) + (y - centerY) * (y - centerY) <= radius * radius;
+                }
+        );
+
+        long result = results.limit(NUMBER_OF_EXPERIMENTS).filter(b -> b).count();
+        return (double) result / NUMBER_OF_EXPERIMENTS;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions
+                .entrySet()
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> entry
+                                        .getValue()
+                                        .stream()
+                                        .mapToInt(String::length)
+                                        .sum()
+                        )
+                )
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingInt(Map.Entry::getValue))
+                .map(Map.Entry::getKey).get();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders
+                .stream()
+                .flatMap(map -> map.entrySet().stream())
+                .collect(
+                        Collectors.groupingBy(
+                                Map.Entry::getKey,
+                                Collectors.collectingAndThen(
+                                        Collectors.toList(),
+                                        list -> list
+                                                .stream()
+                                                .mapToInt(Map.Entry::getValue)
+                                                .sum()
+                                )
+                        )
+                );
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -2,27 +2,99 @@ package ru.spbau.mit;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static ru.spbau.mit.SecondPartTasks.*;
 
 public class SecondPartTasksTest {
-
+    private final static double EPS = 0.01;
     @Test
-    public void testFindQuotes() {
-        fail();
+    public void testFindQuotes() throws Exception {
+        List<String> expected = Arrays.asList(
+                "Slow death, immense decay",
+                "Smell your death as it burns",
+                "His face of death staring down,",
+                "Approaching the altar of death",
+                "Satan's slaughter, ceremonial death",
+                "Eyes dripping blood realization of death",
+                "Holds the key to his own death",
+                "Cold touch of death begins to chill your spine",
+                "My sinful glare at nothing holds thoughts of death behind it",
+                "Vessels in my brain carry death until my birth",
+                "What I am, what I want, I'm only after death"
+        );
+        List<String> paths = Arrays.asList(
+                "src/test/resources/test1.txt",
+                "src/test/resources/test2.txt",
+                "src/test/resources/test3.txt"
+        );
+        List<String> actual = findQuotes(paths, "death");
+        assertArrayEquals(expected.toArray(), actual.toArray());
     }
 
     @Test
-    public void testPiDividedBy4() {
-        fail();
+    public void testPiDividedBy4() throws Exception {
+        assertEquals(Math.PI / 4, piDividedBy4(), EPS);
     }
 
     @Test
-    public void testFindPrinter() {
-        fail();
+    public void testFindPrinter() throws Exception {
+        List<String> compositions1 = Arrays.asList(
+                "foo",
+                "bar",
+                "baz"
+        );
+        List<String> compositions2 = Arrays.asList(
+                "foo",
+                "baz"
+        );
+        List<String> compositions3 = Arrays.asList(
+                "boo",
+                "goo"
+        );
+        Map<String, List<String>> testCompositions = new HashMap<>();
+        testCompositions.put("A", compositions1);
+        testCompositions.put("B", compositions2);
+        testCompositions.put("C", compositions3);
+
+        String expected = "A";
+        String actual = findPrinter(testCompositions);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void testCalculateGlobalOrder() {
-        fail();
+    public void testCalculateGlobalOrder() throws Exception {
+        Map<String, Integer> order1 = new HashMap<>();
+        order1.put("spam", 10);
+        order1.put("eggs", 10);
+
+        Map<String, Integer> order2 = new HashMap<>();
+        order2.put("eggs", 12);
+        order2.put("bacon", 12);
+        order2.put("ham", 12);
+
+        Map<String, Integer> order3 = new HashMap<>();
+        order3.put("eggs", 20);
+
+        List<Map<String, Integer>> testOrders = Arrays.asList(
+                order1,
+                order2,
+                order3
+        );
+
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("spam", 10);
+        expected.put("eggs", 42);
+        expected.put("bacon", 12);
+        expected.put("ham", 12);
+
+
+        Map<String, Integer> actual = calculateGlobalOrder(testOrders);
+        assertEquals(expected, actual);
     }
 }
